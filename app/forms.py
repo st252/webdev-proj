ae3de8cc75fe3fabb7559caa8aa840ba9454a509
ST <166231158+st252@users.noreply.github.com>
2024-05-12 23:08:27 +0800

    Bio editing and display, changed about_me to bio

--- app/forms.py
+++ app/forms.py
@@ -1,6 +1,6 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, BooleanField, SubmitField
-from wtforms.validators import DataRequired, ValidationError, EqualTo
+from wtforms import StringField, PasswordField, BooleanField, SubmitField, TextAreaField
+from wtforms.validators import DataRequired, ValidationError, EqualTo, Length
 from app.models import User
 
 class LoginForm(FlaskForm):
@@ -20,3 +20,18 @@ class RegistrationForm(FlaskForm):
         user = User.query.filter_by(username=username.data).first()
         if user is not None:
             raise ValidationError('Please use a different username.')
+
+class EditProfileForm(FlaskForm):
+    username = StringField('Username', validators=[DataRequired()])
+    bio = TextAreaField('Bio', validators=[Length(min=0, max=256)])
+    submit = SubmitField('Submit')
+
+    def __init__(self, original_username, *args, **kwargs):
+        super(EditProfileForm, self).__init__(*args, **kwargs)
+        self.original_username = original_username
+        
+    def validate_username(self, username):
+        if username.data != self.original_username:
+            user = User.query.filter_by(username=self.username.data).first()
+            if user is not None:
+                raise ValidationError('Please use a different username.')
--- app/routes.py
+++ app/routes.py
@@ -2,7 +2,7 @@ from flask import render_template, flash, redirect, url_for, request
 from app import app, db
 from flask_login import current_user, login_user, logout_user, login_required
 from app.models import User
-from app.forms import LoginForm, RegistrationForm
+from app.forms import EditProfileForm, LoginForm, RegistrationForm
 from urllib.parse import urlsplit
 from datetime import datetime, timezone
 
@@ -68,3 +68,18 @@ def user(username):
         {'artist': {'username': 'bubble'}, 'body':'/img/tired.png', 'parent':2 }
     ]
     return render_template('user.html', user=user, requests=requests, replies=replies)
+
+@app.route('/edit_profile', methods=['GET', 'POST'])
+@login_required
+def edit_profile():
+  form = EditProfileForm(current_user.username)
+  if form.validate_on_submit():
+    current_user.username = form.username.data
+    current_user.bio = form.bio.data
+    db.session.commit()
+    flash('Your changes have been saved.')
+    return redirect(url_for('edit_profile'))
+  elif request.method == 'GET':
+    form.username.data = current_user.username
+    form.bio.data = current_user.bio
+  return render_template('edit_profile.html', title='Edit Profile', form=form)
