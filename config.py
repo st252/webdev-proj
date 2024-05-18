7e3666581f20302f60523df3ae9723402bb62384
Kiki <162666984+kik-ki@users.noreply.github.com>
2024-05-18 18:00:29 +0800

    Added subclasses of Config

--- config.py
+++ config.py
@@ -7,3 +7,10 @@ class Config(object):
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
     SECRET_KEY = os.environ.get('GROUP_PROJECT_SECRET_KEY')
+
+class DeploymentConfig(Config):
+    SQLALCHEMY_DATABASE_URI = "sqlite:///" + os.path.join(basedir, 'app.db')
+
+class TestConfig(Config):
+    SQLALCHEMY_DATABASE_URI = "sqlite:///:memory"
+    TESTING = True
