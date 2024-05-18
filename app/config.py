d58b92d1f3a22762652602f719c3fc015231845c
Kiki <162666984+kik-ki@users.noreply.github.com>
2024-05-19 00:00:03 +0800

    Updated config.py

--- app/config.py
+++ app/config.py
@@ -2,8 +2,6 @@ import os
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config(object):
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
-        'sqlite:///' + os.path.join(basedir, 'app.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
     SECRET_KEY = os.environ.get('GROUP_PROJECT_SECRET_KEY')
