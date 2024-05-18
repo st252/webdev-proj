4f5b58e682665a2a1c2be096c5c08d1b12402144
Kiki <162666984+kik-ki@users.noreply.github.com>
2024-05-18 19:24:46 +0800

    Added deployment config

--- illust.py
+++ illust.py
@@ -1,5 +1,10 @@
 from app import app, db
 from app.models import *
+from flask_migrate import Migrate
+from app.config import DeploymentConfig
+
+app = create_app(DeploymentConfig)
+migrate = Migrate(app, db)
 
 @app.shell_context_processor
 def make_shell_context():
