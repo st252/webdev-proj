bc3f10ad89aac81caa7bbf61fa6607fcd3349b39
Kiki <162666984+kik-ki@users.noreply.github.com>
2024-05-18 18:16:02 +0800

    Registered blueprint

--- app/__init__.py
+++ app/__init__.py
@@ -4,11 +4,20 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_login import LoginManager
 
-app = Flask(__name__)
-app.config.from_object(Config)
-db = SQLAlchemy(app)
+db = SQLAlchemy()
 migrate = Migrate(app, db)
 login = LoginManager(app)
 login.login_view = 'login'
+
+def create_app(config):
+  app = Flask(__name__)
+  app.config.from_object(Config)
+
+  from app.blueprints import main
+  app.register_blueprint(main)
+  db.init_app(app)
+  login.init_app(app)
+
+  return app
 
 from app import routes, models
