152bb113960c8964906b05ff2b6969d08e1047e1
Kiki <162666984+kik-ki@users.noreply.github.com>
2024-05-18 18:04:09 +0800

    Created file for unit tests

---
+++ app/test/unit.py
@@ -0,0 +1,15 @@
+from unittest import TestCase
+from app import create_app, db
+from app.config import TestConfig
+
+class BasicUnitTests(TestCase):
+    def setUp(self):
+        testApp = create_app(TestConfig)
+        self.app_context = testApp.app_context
+        self.app_context.push()
+        db.create_all()
+        
+    def tearDown(self):
+        db.session.remove()
+        db.drop_all()
+        self.app_context.pop()
